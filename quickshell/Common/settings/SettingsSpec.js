851f9b2da3802c9ea41b4e474692b12c5267d908
ArijanJ <arijanj@proton.me>
2026-02-05 22:49:24 +0100

    feat: media playback OSD

--- quickshell/Common/settings/SettingsSpec.js
+++ quickshell/Common/settings/SettingsSpec.js
@@ -330,6 +330,7 @@ var SPEC = {
     osdPosition: { def: 5 },
     osdVolumeEnabled: { def: true },
     osdMediaVolumeEnabled: { def: true },
+    osdMediaPlaybackEnabled: { def: true },
     osdBrightnessEnabled: { def: true },
     osdIdleInhibitorEnabled: { def: true },
     osdMicMuteEnabled: { def: true },
