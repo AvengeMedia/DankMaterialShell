20e94e3d5de611f313e0727e7149fdac068a2f15
chimera <xeniart@tuta.io>
2026-02-10 18:01:22 -0500

    " "

--- distro/nix/greeter.nix
+++ distro/nix/greeter.nix
@@ -13,7 +13,7 @@ let
 
   compositorPackage =
     let
-      compositor = if config.programs.mango.enable != null then mango else cfg.compositor.name;
+      compositor = if config.programs.mango.enable != null then "mango" else cfg.compositor.name;
       configured = lib.attrByPath [ "programs" compositor "package" ] null config;
     in
     if configured != null then configured else builtins.getAttr cfg.compositor.name pkgs;
