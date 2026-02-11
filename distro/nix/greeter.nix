dbe0537f3b52fbbb5c5a14358f9c04fed5bb2307
chimera <xeniart@tuta.io>
2026-02-10 19:40:55 -0500

    mango dependency remove too
    i have got to add "parenthesis" to stuff more

--- distro/nix/greeter.nix
+++ distro/nix/greeter.nix
@@ -13,7 +13,7 @@ let
 
   compositorPackage =
     let
-      compositor = lib.attrByPath [ "programs" "mango" "enable" ] null mango;
+      compositor = lib.attrByPath [ "programs" "mango" "enable" ] null "mango";
       configured = lib.attrByPath [ "programs" compositor "package" ] null config;
     in
     if configured != null then configured else builtins.getAttr cfg.compositor.name pkgs;
