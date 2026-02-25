47b12d09fc8526f9c231de60848a41b5990b4a37
bbedward <bbedward@gmail.com>
2026-02-25 10:45:42 -0500

    Revert "nix: update vendorHash for go.mod changes"
    This reverts commit a2c89e0a8c2aec96a081c1c5cfb95b8e19ede12f.

--- flake.nix
+++ flake.nix
@@ -80,7 +80,7 @@
               inherit version;
               pname = "dms-shell";
               src = ./core;
-              vendorHash = "sha256-dEk7IOd6aQwaxZruxQclN7TGMyb8EJOl6NBWRsoZ9HQ=";
+              vendorHash = "sha256-cVUJXgzYMRSM0od1xzDVkMTdxHu3OIQX2bQ8AJbGQ1Q=";
 
               subPackages = [ "cmd/dms" ];
 
