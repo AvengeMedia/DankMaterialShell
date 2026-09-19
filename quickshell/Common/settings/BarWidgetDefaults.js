ab31c88010ac7767cfab702e794f0b9c6914ae73
fion <neofion@proton.me>
2026-09-19 23:35:48 +0400

    keyboard layout: add user-editable label overrides for compact mode

--- quickshell/Common/settings/BarWidgetDefaults.js
+++ quickshell/Common/settings/BarWidgetDefaults.js
@@ -123,7 +123,8 @@ var DEFAULTS = {
     },
     keyboard_layout_name: {
         keyboardLayoutNameCompactMode: false,
-        keyboardLayoutNameShowIcon: false
+        keyboardLayoutNameShowIcon: false,
+        keyboardLayoutNameLabelOverrides: {}
     },
     cpuUsage: {
         minimumWidth: true
